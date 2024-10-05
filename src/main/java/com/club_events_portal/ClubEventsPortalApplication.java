22e71decf6f7551fee789e4d3704b210c3fdf268
karthiksrinivas19 <karthikdp2005@gmail.com>
2024-10-05 13:13:38 +0530

    Initial commit

---
+++ src/main/java/com/club_events_portal/ClubEventsPortalApplication.java
@@ -0,0 +1,13 @@
+package com.club_events_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubEventsPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubEventsPortalApplication.class, args);
+	}
+
+}
